fb616f6509d6925168d61eeb473cb0e67f1677af
javicerda <javicerda.z@gmail.com>
2020-06-17 23:31:36 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-content>
+      <nav-bar/>
+    </v-content>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+
+  components: {
+    NavBar,
+    Footer
+  },
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+   <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+    >
+    <v-spacer></v-spacer>
+      <v-card-title class="text-center">
+        <strong class="subheading">Síguenos!</strong>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          dark
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>pan.ico</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-instagram',
+      ],
+    }),
+  }
+</script>
